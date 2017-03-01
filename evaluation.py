b6ab72bbafc7704b49fec5ea6b609464c0b3c336
IshitaTakeshi <ishitah.takeshi@gmail.com>
2017-03-01 11:17:25 +0000

    Add an evaluation script

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,13 @@
 from os.path import exists, join
 import pickle
+import json
 import gzip
 from urllib.request import urlretrieve
+from argparse import ArgumentParser
+import hashlib
+import time
+import timeit
+from multiprocessing import cpu_count
 
 import numpy as np
 from mnist import MNIST
@@ -11,28 +17,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
+
 from pcanet import PCANet
+from ensemble import Bagging
 
 
 def load_mnist():
-    # mnist = fetch_mldata("MNIST original", data_home=".")
-    # print(mnist["target"].shape)
-    # print(mnist["data"].shape)
-    # exit(0)
-    # url = "http://deeplearning.net/data/mnist/mnist.pkl.gz"
-    # mnist_compressed = "mnist.pkl.gz"
-
-    # if not exists(mnist_compressed):
-    #     print("Downloading MNIST")
-    #     urlretrieve(url, mnist_compressed)
-
-    # # Load the dataset
-    # with gzip.open(mnist_compressed, "rb") as f:
-    #     u = pickle._Unpickler(f)
-    #     u.encoding = "latin1"
-    #     data = u.load()
-    # data = [(X.reshape(-1, 28, 28), y) for X, y in data]
-
     mnist = MNIST("mnist")
     X_train, y_train = mnist.load_training()
     X_test, y_test = mnist.load_testing()
@@ -51,54 +41,176 @@ def params_to_str(params):
     return "_".join([key + "_" + str(params[key]) for key in keys])
 
 
-if __name__ == "__main__":
-    train_set, test_set = load_mnist()
+def run_classifier(X_train, X_test, y_train, y_test):
+    model = RandomForestClassifier(n_estimators=100, random_state=1234, n_jobs=-1)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    return y_test, y_pred
 
+
+def run_pcanet_normal(estimator_params,
+                      images_train, images_test, y_train, y_test):
+    model = PCANet(**estimator_params)
+    model.validate_structure()
+
+    t1 = timeit.default_timer()
+    model.fit(images_train)
+    t2 = timeit.default_timer()
+    training_time = t2 - t1
+
+    X_train = model.transform(images_train)
+    X_test = model.transform(images_test)
+
+    y_test, y_pred = run_classifier(X_train, X_test, y_train, y_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    return model, accuracy, training_time
+
+
+# TODO Change n_estimators and sampling_ratio on evaluation
+def run_pcanet_ensemble(ensemble_params, estimator_params,
+                        images_train, images_test, y_train, y_test):
+    model = Bagging(
+        n_estimators=ensemble_params["n_estimators"],
+        sampling_ratio=ensemble_params["sampling_ratio"],
+        n_jobs=ensemble_params["n_jobs"],
+        **estimator_params)
+
+    t1 = timeit.default_timer()
+    model.fit(images_train)
+    t2 = timeit.default_timer()
+    training_time = t2 - t1
+
+    X_train = model.transform(images_train)
+    X_test = model.transform(images_test)
+
+    y_test, y_pred = run_classifier(X_train, X_test, y_train, y_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    return model, accuracy, training_time
+
+
+def parse_args():
+    parser = ArgumentParser()
+
+    parser.add_argument("--image-shape", dest="image_shape", type=int,
+            required=True)
+    parser.add_argument("--filter-shape-l1", dest="filter_shape_l1", type=int,
+            required=True)
+    parser.add_argument("--step-shape-l1", dest="step_shape_l1", type=int,
+            required=True)
+    parser.add_argument("--n-l1-output", dest="n_l1_output", type=int,
+            required=True)
+    parser.add_argument("--filter-shape-l2", dest="filter_shape_l2", type=int,
+            required=True)
+    parser.add_argument("--step-shape-l2", dest="step_shape_l2", type=int,
+            required=True)
+    parser.add_argument("--n-l2-output", dest="n_l2_output", type=int,
+            required=True)
+    parser.add_argument("--block-shape", dest="block_shape", type=int,
+            required=True)
+    parser.add_argument("--n-estimators", dest="n_estimators", type=int,
+            required=True)
+    parser.add_argument("--sampling-ratio", dest="sampling_ratio", type=float,
+            required=True)
+    parser.add_argument("--n-jobs", dest="n_jobs", type=int,
+            required=True)
+    return parser.parse_args()
+
+
+def save_model(model, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(model, f)
+
+
+def model_filename():
+    t = str(time.time()).encode("utf-8")
+    return hashlib.sha256(t).hexdigest() + ".pkl"
+
+
+def pick(train_set, test_set, n_train, n_test):
     images_train, y_train = train_set
     images_test, y_test = test_set
+    train_set = images_train[:n_train], y_train[:n_train]
+    test_set = images_test[:n_test], y_test[:n_test]
+    return train_set, test_set
 
-    n_train = len(y_train)
-    n_test = len(y_test)
 
-    images_train, y_train = images_train[:n_train], y_train[:n_train]
-    images_test, y_test = images_test[:n_test], y_test[:n_test]
+def run_evaluation(datasize, estimator_params, ensemble_params):
+    # params = vars(parse_args())
 
-    params = {
+    pickle_dir = "pickles"
+
+    n_train, n_test = datasize["n_train"], datasize["n_test"]
+
+    train_set, test_set = load_mnist()
+    train_set, test_set = pick(train_set, test_set, n_train, n_test)
+    (images_train, y_train), (images_test, y_test) = train_set, test_set
+
+    params = dict(
+        list(estimator_params.items()) +
+        list(ensemble_params.items())
+    )
+
+    params["n_train"] = n_train
+    params["n_test"] = n_test
+
+    model, accuracy, training_time = run_pcanet_normal(
+        estimator_params,
+        images_train, images_test, y_train, y_test
+    )
+
+    filename = model_filename()
+    save_model(model, join(pickle_dir, filename))
+    params["normal-model"] = filename
+    params["normal-accuracy"] = accuracy
+    params["normal-training-time"] = training_time
+
+    model, accuracy, training_time = run_pcanet_ensemble(
+        ensemble_params, estimator_params,
+        images_train, images_test, y_train, y_test
+    )
+
+    filename = model_filename()
+    save_model(model, join(pickle_dir, filename))
+    params["ensemble-model"] = filename
+    params["ensemble-accuracy"] = accuracy
+    params["ensemble-training-time"] = training_time
+
+    with open("result.json", "a") as f:
+        json.dump(params, f, indent=2, sort_keys=True)
+
+
+if __name__ == "__main__":
+    datasize = {
+        "n_train": None,
+        "n_test": None
+    }
+    estimator_params = {
         "image_shape": 28,
         "filter_shape_l1": 4, "step_shape_l1": 2, "n_l1_output": 3,
         "filter_shape_l2": 4, "step_shape_l2": 1, "n_l2_output": 3,
         "block_shape": 5
     }
+    ensemble_params = {
+        "n_estimators": 10,
+        "sampling_ratio": 0.3,
+        "n_jobs": cpu_count()
+    }
 
-    # from ensemble import Bagging
-    # model = Bagging(
-    #     n_estimators=400,
-    #     sampling_ratio=0.3,
-    #     n_jobs=4,
-    #     image_shape=28,
-    #     filter_shape_l1=2, step_shape_l1=1, n_l1_output=4,
-    #     filter_shape_l2=2, step_shape_l2=1, n_l2_output=4,
-    #     block_shape=2)
+    n_estimators_list = [
+        10, 20, 30, 40, 50, 60, 70, 80, 90,
+        100, 200, 300, 400, 500, 600, 700, 800, 900,
+        1000
+    ]
 
-    model = PCANet(**params)
-    model.validate_structure()
-    model.fit(images_train)
-    X_train = model.transform(images_train)
-    X_test = model.transform(images_test)
-    del(model)
-    del(images_train)
-    del(images_test)
+    sampling_ratio_list = [
+        0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
+        0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9
+    ]
 
-    model = RandomForestClassifier(n_estimators=100, random_state=1234, n_jobs=-1)
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Accuracy: " + str(accuracy))
-
-    filename = (params_to_str(params) +
-                "_n_train_" + str(n_train) +
-                "_n_test_" + str(n_test) +
-                "_accuracy_" + str(accuracy) + ".pkl")
-
-    with open(join("pickles", filename), "wb") as f:
-        pickle.dump(model, f)
+    for n_estimators in n_estimators_list:
+        for sampling_ratio in sampling_ratio_list:
+            ensemble_params["n_estimators"] = n_estimators
+            ensemble_params["sampling_ratio"] = sampling_ratio
+            run_evaluation(datasize, estimator_params, ensemble_params)
