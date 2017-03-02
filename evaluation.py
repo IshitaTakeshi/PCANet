1ce905f34cea1027a514739b516917f01b99db4c
IshitaTakeshi <ishitah.takeshi@gmail.com>
2017-03-02 06:51:17 +0000

    Rewrite training attempt loops with numpy

--- evaluation.py
+++ evaluation.py
@@ -198,19 +198,14 @@ if __name__ == "__main__":
         "n_jobs": cpu_count()
     }
 
-    n_estimators_list = [
-        10, 20, 30, 40, 50, 60, 70, 80, 90,
-        100, 200, 300, 400, 500, 600, 700, 800, 900,
-        1000
-    ]
-
     sampling_ratio_list = [
         0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
-        0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9
+        0.1,
     ]
 
-    for n_estimators in n_estimators_list:
-        for sampling_ratio in sampling_ratio_list:
-            ensemble_params["n_estimators"] = n_estimators
-            ensemble_params["sampling_ratio"] = sampling_ratio
+    for sampling_ratio in np.arange(0.01, 0.11, 0.01).astype(float):
+        for n_estimators in np.arange(10, 210, 10).astype(int):
+            # convert from numpy object
+            ensemble_params["n_estimators"] = int(n_estimators)
+            ensemble_params["sampling_ratio"] = float(sampling_ratio)
             run_evaluation(datasize, estimator_params, ensemble_params)
