8a9149fd2431d6bf1ef29439b4c1a79a0a043d16
takeshi-ishita <takeshi-ishita@cookpad.jp>
2017-03-05 16:04:11 +0900

    Use LinearSVC to predict labels in each estimator

--- ensemble.py
+++ ensemble.py
@@ -1,60 +1,80 @@
 from multiprocessing import Pool
 from itertools import repeat
 
+from sklearn.svm import LinearSVC
+from sklearn.externals.joblib import Parallel, delayed
 from numpy.random import randint
 from pcanet import PCANet
 import numpy as np
 from tqdm import tqdm
 
 
+def most_frequent_label(v):
+    values, counts = np.unique(v, return_counts=True)
+    return values[np.argmax(counts)]
 
-def transform(estimator, images):
-    return estimator.transform(images)
+
+def predict(transformer, estimator, images):
+    X = transformer.transform(images)
+    y = estimator.predict(X)
+    return y
 
 
-def fit_random(estimator, images, sampling_ratio):
+def fit(transformer, estimator, images, y):
+    transformer.fit(images)
+    X = transformer.transform(images)
+    estimator.fit(X, y)
+    return transformer, estimator
+
+
+def fit_random(transformer, estimator, images, y, sampling_ratio):
     n_images = images.shape[0]
     n_samples = int(n_images * sampling_ratio)
     indices = randint(0, n_images, n_samples)
-    estimator.fit(images[indices])
-    return estimator
+    return fit(transformer, estimator, images[indices], y[indices])
 
 
 class Bagging(object):
     def __init__(self, n_estimators, sampling_ratio, n_jobs,
-                 **estimator_params):
+                 **transformer_params):
         """
         n_estimators: int
+            The number of estimators
         sampling_ratio: int
-            The number of samples to draw from X to train each base estimator.
+            The number of samples to draw from X to train each base transformer.
         n_jobs: int
             The number of jobs to run in parallel.
             The number of cores is set if -1.
-        estimator_params: dict
+        transformer_params: dict
             Parameters for PCANet.__init__
         """
 
+        self.transformers = \
+            [PCANet(**transformer_params) for i in range(n_estimators)]
+        # Validate only the first transformer
+        # since all of transformers share the same hyperparameters
+        self.transformers[0].validate_structure()
         self.estimators = \
-            [PCANet(**estimator_params) for i in range(n_estimators)]
-        # Validate only the first estimator
-        # since all of estimators share the same hyperparameters
-        self.estimators[0].validate_structure()
+            [LinearSVC(C=10) for i in range(n_estimators)]
 
         self.sampling_ratio = sampling_ratio
         self.n_jobs = n_jobs
 
-    def fit(self, images):
-        # run fit_random(estimator, images, sampling_ratio) in parallel
-        args = zip(self.estimators,
-                   repeat(images),
-                   repeat(self.sampling_ratio))
+    def fit(self, images, y):
+        # run fit_random in parallel
+        g = zip(self.transformers,
+                self.estimators,
+                repeat(images),
+                repeat(y),
+                repeat(self.sampling_ratio))
+        t = Parallel(n_jobs=self.n_jobs)(delayed(fit_random)(*a) for a in g)
+        self.transformers, self.estimators = zip(*t)
+        return self
 
-        with Pool(self.n_jobs) as pool:
-            self.estimators = pool.starmap(fit_random, args)
-        return self.estimators
-
-    def transform(self, images):
-        # run transform(estimator, images) in parallel
-        with Pool(self.n_jobs) as pool:
-            X = pool.starmap(transform, zip(self.estimators, repeat(images)))
-        return np.mean(X, axis=0)  # merge the results
+    def predict(self, images):
+        # run transform(transformer, images) in parallel
+        g = zip(self.transformers, self.estimators, repeat(images))
+        Y = Parallel(n_jobs=self.n_jobs)(delayed(predict)(*a) for a in g)
+        Y = np.array(Y)  # Y is of shape (n_estimators, n_classes)
+        y = [most_frequent_label(Y[:, i]) for i in range(Y.shape[1])]
+        return np.array(y)
